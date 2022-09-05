3f4b18380086cf0a02815809a34db10b6125852e
Huzaifa-2609 <huzaifa.oct@gmail.com>
2022-09-05 22:56:31 +0500

    spa part 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Card, CardBody, CardSubtitle, CardText, CardTitle } from "reactstrap";
 
-const Home = () => {
+const RenderCard = ({ item }) => {
   return (
-    <div className='container'>
-      <h4>Home</h4>
-    </div>
-  )
-}
+    <Card
+      style={{
+        width: "100%",
+      }}
+    >
+      <img alt={item.name} src={item.image} />
+      <CardBody>
+        <CardTitle tag="h5">{item.name}</CardTitle>
 
-export default Home
+        {item.designation && (
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            {item.designation}
+          </CardSubtitle>
+        )}
+
+        <CardText>
+          {item.description}
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
+const Home = (props) => {
+  return (
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dishes} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,58 @@
-import React, { Component } from 'react';
-import Menu from './Menu';
-import { DISHES } from '../shared/dishes';
-import Header from './Header';
-import Footer from './Footer';
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Home from './Home';
+import React, { Component } from "react";
+import Menu from "./Menu";
+import { DISHES } from "../shared/dishes";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Home";
+import { LEADERS } from "../shared/leader";
+import { PROMOTIONS } from "../shared/promotions";
+import { COMMENTS } from "../shared/comments";
+import Contact from "./Contact";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          dishes: DISHES,
-        };
-      }
-      onSelectDish = (dishid) => {
-        this.setState({
-          selectedDish: dishid,
-        });
-      };
-      render() {
-        
-
-        
-        return (
-          <div>
-            <Header/>
-            <Routes>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/menu" element={<Menu dishes={this.state.dishes}/>}/>
-            <Route
-        path="*"
-        element={<Navigate to="/home" replace />}
-    />
-            
-            </Routes>
-            {/* <DishDetails dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/> */}
-            <Footer/>
-          </div>
-        );
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      leader: LEADERS,
+      promotion: PROMOTIONS,
+      comments: COMMENTS,
+    };
+  }
+  onSelectDish = (dishid) => {
+    this.setState({
+      selectedDish: dishid,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <Home
+                dishes={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={
+                  this.state.promotion.filter((promo) => promo.featured)[0]
+                }
+                leader={
+                  this.state.leader.filter((leader) => leader.featured)[0]
+                }
+              />
+            }
+          />
+          <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+          <Route path="/contactus" element={<Contact/>} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+        {/* <DishDetails dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/> */}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
