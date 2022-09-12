7af5532e7c163b119b592a47609cfe43c6c310c8
Huzaifa-2609 <huzaifa.oct@gmail.com>
2022-09-12 22:36:03 +0500

    Combinereducers

--- src/redux/configuer.js
+++ src/redux/configuer.js
@@ -1,10 +1,19 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Comments } from './comments';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Promotions } from './promotion';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        // Reducer, // reducer
+        // initialState, // our initialState
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments,
+            leaders:Leaders,
+            pormotions:Promotions,
+        })
     );
 
     return store;
