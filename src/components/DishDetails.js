9b168d9ad4d28fa65eaa59b0043cd336fab20e25
Huzaifa-2609 <huzaifa.oct@gmail.com>
2022-09-12 21:02:20 +0500

    assignment 3 done

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,18 +1,136 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardText, CardTitle } from "reactstrap";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Button, Label, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
 
+// Commentform Component  starts
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+  }
+  toggleModal = () => {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  };
+  handleSubmit = (values) => {
+    alert("the current state is " + JSON.stringify(values));
+    console.log("the current state is " + JSON.stringify(values));
+  };
+
+  render() {
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !val || val.length <= len;
+    const minLength = (len) => (val) => val && val.length >= len;
+    return (
+      <div>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm
+              className="container"
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
+              <Row>
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select
+                  className="form-control"
+                  model=".rating"
+                  id="rating"
+                  type="select"
+                  name="rating"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </Row>
+              <Row className="form-group" md={10}>
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text
+                  className="form-control"
+                  model=".author"
+                  id="author"
+                  name="author"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="message">Comment</Label>
+                <Control.textarea
+                  model=".message"
+                  id="message"
+                  name="message"
+                  rows="6"
+                  className="form-control"
+                />
+              </Row>
+              <Button type="submit" value="submit" color="primary">
+                Submit
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+      </div>
+    );
+  }
+}
+// Commentform component ends
+
+// Dish Details working start
 const RenderComments = ({ comments }) => {
   return (
-    <li className="mb-3" >
-      <h5>{comments.comment}</h5>
-      <h5>
-        <span> --{comments.author}</span>,{" "}
-        <span className="mx-1">
-          {new Intl.DateTimeFormat("en-US").format(Date.parse(comments.date))}
-        </span>
-      </h5>
-    </li>
+    <>
+      <h1>Comments</h1>
+      {comments.map((comm) => {
+        return (
+          <ul className="mt-2 list-unstyled">
+            <li className="mb-3">
+              <h5>{comm.comment}</h5>
+              <h5>
+                <span> --{comm.author}</span>,{" "}
+                <span className="mx-1">
+                  {new Intl.DateTimeFormat("en-US").format(
+                    Date.parse(comm.date)
+                  )}
+                </span>
+              </h5>
+            </li>
+          </ul>
+        );
+      })}
+      <CommentForm />
+    </>
   );
 };
 
@@ -39,24 +157,21 @@ const DishDetails = ({ dish, comments }) => {
     dish && (
       <div className="container">
         <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{dish.name}</h3>
+            <hr />
+          </div>
+        </div>
         <div className="row">
           <RenderDish dish={dish} />
           <div className="col-12 col-md-5 m-1">
-            <h1>Comments</h1>
-            <ul className="mt-2 list-inline">
-              {comments.map((comm) => {
-                return <RenderComments key={comm.id} comments={comm} />;
-              })}
-            </ul>
+            <RenderComments comments={comments} />;
           </div>
         </div>
       </div>
